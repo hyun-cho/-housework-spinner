c4ff4156ff0784df0ff2eeb6a659a46e3354d2e3
조종현 <hyuncho@nhn.com>
2022-03-07 16:01:03 +0900

    getWeek

--- docs/index.js
+++ docs/index.js
@@ -2,6 +2,42 @@
 
 const root = document.getElementById('root');
 
+Date.prototype.getWeek = function (dowOffset) {
+  /*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
+
+  dowOffset = typeof(dowOffset) == 'number' ? dowOffset : 0; // dowOffset이 숫자면 넣고 아니면 0
+  var newYear = new Date(this.getFullYear(),0,1);
+  var day = newYear.getDay() - dowOffset; //the day of week the year begins on
+  day = (day >= 0 ? day : day + 7);
+  var daynum = Math.floor((this.getTime() - newYear.getTime() -
+    (this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
+  var weeknum;
+  //if the year starts before the middle of a week
+  if(day < 4) {
+    weeknum = Math.floor((daynum+day-1)/7) + 1;
+    if(weeknum > 52) {
+      let nYear = new Date(this.getFullYear() + 1,0,1);
+      let nday = nYear.getDay() - dowOffset;
+      nday = nday >= 0 ? nday : nday + 7;
+      /*if the next year starts before the middle of
+        the week, it is week #1 of that year*/
+      weeknum = nday < 4 ? 1 : 53;
+    }
+  }
+  else {
+    weeknum = Math.floor((daynum+day-1)/7);
+  }
+  return weeknum;
+};
+
 (() => {
-    console.log(root);
+  const date = new Date();
+  const createSpinner = () => {
+    const week = date.getWeek();
+
+
+    return `<div>${week}</div>`
+  }
+
+    root.innerHTML = createSpinner();
 })()
